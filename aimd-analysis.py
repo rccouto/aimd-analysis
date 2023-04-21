bee6369f412155d257faa286ec4e24c2987175d1
Rafael Carvalho Couto <rccoutorafael@gmail.com>
2023-04-21 14:35:12 +0200

    file figs on --analyze torsonly

--- aimd-analysis.py
+++ aimd-analysis.py
@@ -350,13 +350,13 @@ def main():
             sys.path.insert(1, '/Users/rafael/theochem/projects/codes/tcutil/code/geom_param') 
             # LOAD TRAJECTORIE(S)
             topology = md.load_prmtop('sphere.prmtop')
-            traj = md.load_dcd('coors-all.dcd', top = topology)
+            traj = md.load_dcd('coors.dcd', top = topology)
 
         elif arg.analyze == 'torsonly':
             sys.path.insert(1, '/proj/nhlist/users/x_rafca/progs/tcutil/code/geom_param')
             import geom_param as gp
             topology = md.load_prmtop('../sphere.prmtop')
-            traj = md.load_dcd('coors.dcd', top = topology)
+            traj = md.load_dcd('coors-all.dcd', top = topology)
 
         # ON BERZELIUS
         else:
