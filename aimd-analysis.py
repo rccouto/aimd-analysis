fac62476a78196578bf4bc0ef7614c594f5abcc8
Rafael Carvalho Couto <rccoutorafael@gmail.com>
2024-01-02 14:23:17 +0100

    small fixes

--- aimd-analysis.py
+++ aimd-analysis.py
@@ -3111,6 +3111,8 @@ def main():
             sys.path.insert(1, '/home/rcouto/theochem/progs/tcutil/code/geom_param')
         elif socket.gethostname() == "berzelius2.nsc.liu.se":
             sys.path.insert(1, '/proj/berzelius-2023-33/users/x_rafca/progs/tcutil/code/geom_param')
+        elif socket.gethostname() == "amaze":
+            sys.path.insert(1, '/data/users/rcc/codes/tcutil/code/geom_param')
         else:
             sys.path.insert(1, '/Users/rafael/theochem/projects/codes/tcutil/code/geom_param') 
         import geom_param as gp
